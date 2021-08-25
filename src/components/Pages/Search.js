ba5340475b83023860a4231202dd21dada148d9c
diganta15 <ddas461598@gmail.com>
2021-08-25 18:16:06 +0530

    Search feature working

--- src/components/Pages/Search.js
+++ src/components/Pages/Search.js
@@ -1,37 +1,47 @@
-import React,{useState, useContext} from 'react';
+import React,{useState,useEffect, useContext, Fragment} from 'react';
 import SearchContext from "../../context/search/searchContext";
 import Loading from "../layout/Loading";
 import Movies from "../Movies/Movies";
 import Shows from "../shows/Shows";
+import {queries} from "@testing-library/react";
 
 
 
 const Search = () => {
     const searchState = useContext(SearchContext);
     const {loading, searchResultsMovies,searchResultsShows, searchRequest} = searchState;
-    const newMoviesResults = searchResultsMovies.results.slice(0,10);
-    const newShowsResults = searchResultsShows.results.slice(0,10);
+    const [query, setQuery] = useState('');
 
-    const onClick = () => {
+    const onClick = async (e) => {
+        e.preventDefault();
+        await searchRequest(query);
+
+
 
     }
     return (
         <div className='search-wrapper'>
             <div className="search-bar-wrapper">
-                <input type="text" name="query" className="search" placeholder="Search For Movies Or Shows" id=""/>
-                <button className="btn btn-search"><i className="fas fa-search"></i></button>
+                <input type="text" name="query" value={query} className="search" placeholder="Search For Movies Or Shows" id=""
+                onChange={(e)=>setQuery(e.target.value)}
+                />
+                <button className="btn btn-search" onClick={onClick}><i className="fas fa-search"></i></button>
             </div>
 
 
-            {loading? <Loading />:(
+            {loading!==true && searchResultsShows!==null  && searchResultsMovies!== null?(
                <div className="results">
-                   <h1 className="search-title">Movies</h1>
-                   <Movies movies={newMoviesResults} />
-                   <h1 className="search-title">TV Shows</h1>
-                    <Shows shows={newShowsResults} />
+
+                       <Fragment>
+                           <h1 className="search-title">Movies</h1>
+                           <Movies movies={searchResultsMovies.results} />
+                           <h1 className="search-title">TV Shows</h1>
+                           <Shows shows={searchResultsShows.results} />
+                       </Fragment>
+
                </div>
 
-            )}
+            ):<Loading />}
         </div>
     );
 };
